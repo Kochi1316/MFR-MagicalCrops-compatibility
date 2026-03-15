5427040662c9ca3b3b73ff64c82bba564b1ae589
Kochi1316 <147999711+Kochi1316@users.noreply.github.com>
2026-03-15 17:17:16 +0100

    Fix Magical Crops planting on MFR Fertile Soil
    Patched Plantable soil validation so the MFR Planter accepts MFR Fertile Soil in addition to vanilla farmland.

--- src/main/java/portablejim/compatibility/Plantable.java
+++ src/main/java/portablejim/compatibility/Plantable.java
@@ -1,5 +1,6 @@
 package portablejim.compatibility;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -10,7 +11,11 @@ import powercrystals.minefactoryreloaded.api.IFactoryPlantable;
 import powercrystals.minefactoryreloaded.api.ReplacementBlock;
 
 public class Plantable implements IFactoryPlantable {
-    private ItemSeeds seed;
+
+    private final ItemSeeds seed;
+
+    private static final Block MFR_FERTILE_SOIL =
+            GameRegistry.findBlock("MineFactoryReloaded", "farmland");
 
     public Plantable(ItemSeeds seed) {
         this.seed = seed;
@@ -31,25 +36,30 @@ public class Plantable implements IFactoryPlantable {
         return new ReplacementBlock(seed.getPlant(world, x, y, z));
     }
 
+    private boolean isValidSoil(Block ground) {
+        return ground == Blocks.farmland || ground == MFR_FERTILE_SOIL;
+    }
+
     @Override
     public boolean canBePlantedHere(World world, int x, int y, int z, ItemStack stack) {
-        if(!world.isAirBlock(x, y, z))
+        if (!world.isAirBlock(x, y, z)) {
             return false;
+        }
+
         Block ground = world.getBlock(x, y - 1, z);
-        return ground.equals(Blocks.farmland);
+        return isValidSoil(ground);
     }
 
     @Override
     public void prePlant(World world, int x, int y, int z, ItemStack stack) {
         Block ground = world.getBlock(x, y - 1, z);
-        if (ground.equals(Blocks.grass) || ground.equals(Blocks.dirt))
-        {
+
+        if (ground == Blocks.grass || ground == Blocks.dirt) {
             world.setBlock(x, y - 1, z, Blocks.farmland);
         }
     }
 
     @Override
     public void postPlant(World world, int x, int y, int z, ItemStack stack) {
-
     }
 }
